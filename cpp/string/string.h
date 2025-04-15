e0223c5822fe32ec168a4443be825638d5023e8e
bubblerion <windowrains@naver.com>
2025-04-15 14:38:55 +0900

    영재야 사랑해

---
+++ cpp/string/string.h
@@ -0,0 +1,23 @@
+#ifndef STRING_H
+#define STRING_H
+
+class String{
+
+private:
+    char *str;
+    int len;
+
+
+public:
+    String();
+    String(const char *s);
+    String(const String& rhs);
+    ~String();
+
+    const char *c_str();
+    int size();
+
+    
+};
+
+#endif
