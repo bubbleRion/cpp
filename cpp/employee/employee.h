8869195150b1e82027b95156b2d686d4a34ba35f
bubblerion <windowrains@naver.com>
2025-04-15 17:19:57 +0900

    석준아 사랑해

--- cpp/employee/employee.h
+++ cpp/employee/employee.h
@@ -6,15 +6,26 @@ class Employee{
 private:
     int id;
     String name;
-
+    Employee * pManager;
+    //
+    Employee(const Employee& rhs);
+    Employee& operator=(const Employee& rhs);
 public:
-    // Employee() {  }
+    Employee(int id , String name , /* const */ Employee *pManager);
+    // Employee() {  } is not generated!
     // Employee(const Employee& rhs) { /* memberwise copy */}
     // ~Employee() {  }
 
     // Employee& operator=(const Employee& rhs) { /*memberwise copy return *this; */}
     // Employee* operator&() { return this;}
     // const Employee * operator&() const { return this; }
+
+    int getId();
+    String getName();
+    Employee *getManager();
+
+    bool isManager();
+    
 };
 
 #endif
--- cpp/employee/main.cpp
+++ cpp/employee/main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 #include "employee.h"
 
+void printEmployee(Employee *p)
+{
+    std::cout << "id : " << p->getId() << " name : " << p->getName() << " | ";
+    std::cout << ((p->isManager()) ? "" : p->getManager()->getName() ) << std::endl;
+}
+
 int main()
 {
-    
+    /*
+    Employee e1(1, "Park Jung Seok" , NULL);
+    Employee e2(2, "Sung Young Ho" , &e1);
+    Employee e3(3, "Choi Su Gil" , &e1);
+    Employee e4(4, "Cho min sung" , &e1);
+    Employee e5(5, "Min Byung Hun" , &e4);
+    */
+    // 
+
+    // Employee e6 = e1;
+    // e1 = e2;
+
+    // Employee Employees[10]; // X default constructor is called.
+    Employee* pEmployees[10];
+    pEmployees[0] = new Employee(1, "Park Jung Seok" , NULL);
+    pEmployees[1] = new Employee(2, "Sung Young Ho" , pEmployees[0]);
+    pEmployees[2] = new Employee(3, "Choi Su Gil" , pEmployees[0]);
+    pEmployees[3] = new Employee(4, "Cho min sung" , pEmployees[0]);
+    pEmployees[4] = new Employee(5, "Min Byung Hun" , pEmployees[3]);
+    for(int i = 0; i < 5; ++i){
+        printEmployee(pEmployees[i]);
+    }
+
+    for(int i = 0; i < 5; ++i)
+    {
+        delete pEmployees[i];
+    }
+
+    // std::cout << "id: " << e1.getId() << " " << e1.getName() <<  std::endl;
+    // std::cout << "id: " << e2.getId() << " " << e2.getName() <<  std::endl;
     return 0;
 }
