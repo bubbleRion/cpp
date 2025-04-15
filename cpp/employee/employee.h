9ef77cf1d0e1a858e979af75e8190a91b90919b6
bubblerion <windowrains@naver.com>
2025-04-15 16:13:30 +0900

    급하게 올린다야

---
+++ cpp/employee/employee.h
@@ -0,0 +1,20 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+#include "string.h"
+class Employee{
+
+private:
+    int id;
+    String name;
+
+public:
+    // Employee() {  }
+    // Employee(const Employee& rhs) { /* memberwise copy */}
+    // ~Employee() {  }
+
+    // Employee& operator=(const Employee& rhs) { /*memberwise copy return *this; */}
+    // Employee* operator&() { return this;}
+    // const Employee * operator&() const { return this; }
+};
+
+#endif
---
+++ cpp/employee/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "employee.h"
+
+int main()
+{
+    
+    return 0;
+}
