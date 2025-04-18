667d2e7440af4c8f7235644de8dd3cfde76bc0f8
bubblerion <windowrains@naver.com>
2025-04-18 10:56:27 +0900

    영재야...

---
+++ cpp/safeArray/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "safeArray.h"
+
+int main()
+{
+    SafeArray arr1;
+    SafeArray arr2(10);
+    int nums[] = {1 ,2, 3, 4, 5};
+    SafeArray arr3(nums , 5);
+    SafeArray arr4 = arr3;
+
+    arr1 = arr3;
+
+    arr1 == arr3;
+
+    for (int i = 0; i < arr1.size(); ++i)
+    {
+        std::cout << arr1[i] << std::endl;
+    }
+
+    arr1[5] = 6; // boundary error!
+
+    return 0;
+}
